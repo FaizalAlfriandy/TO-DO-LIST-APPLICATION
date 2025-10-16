f21a9492e9a586722f3b238cfa4887ceecc2fc85
FaizalAlfriandy <mfaizalalfriandy@gmail.com>
2025-10-16 16:18:46 +0700

    Initial Commit

--- app/src/main/java/com/faizal/apptodolist/TaskViewModel.kt
+++ app/src/main/java/com/faizal/apptodolist/TaskViewModel.kt
@@ -2,9 +2,32 @@ package com.faizal.apptodolist
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.time.LocalDate
+import java.time.LocalTime
+import java.util.UUID
 
 class TaskViewModel: ViewModel()
 {
-    var name = MutableLiveData<String>()
-    var desc = MutableLiveData<String>()
+    var taskItems = MutableLiveData<MutableList<TaskItem>>()
+
+    init {
+        taskItems.value = mutableListOf()
+    }
+
+    fun addTaskItem(newTask: TaskItem)
+    {
+        val list = taskItems.value
+        list!!.add(newTask)
+        taskItems.postValue(list)
+    }
+
+    fun updateTaskItem(id: UUID, name: String, desc: String, dueTime: LocalTime?, completeDate: LocalDate?)
+    {
+        val list = taskItems.value
+        val task = list!!.find { it.id == id }!!
+        task.name = name
+        task.desc = desc
+        task.dueTime = dueTime
+        taskItems.postValue(list)
+    }
 }
